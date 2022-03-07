e37f9b698ddbb89b4d3d817a38bec626f57ac2c2
damajojung <jungdamajo@gmail.com>
2022-03-07 15:32:52 +0100

    Saving

--- packages.R
+++ packages.R
@@ -1,6 +1,21 @@
+# 
 
 install.packages("pcalg")
-install.packages("kpcalg")
-install.packages("devtools")
+install.packages("RBGL")
+# install.packages("kpcalg")
+# install.packages("devtools")
 library(devtools)
-install_github("Diviyan-Kalainathan/RCIT")
+install_github("Diviyan-Kalainathan/RCIT", force = TRUE)
+
+
+library(RCIT)
+library(pcalg)
+library(kpcalg)
+library(devtools)
+
+
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("RBGL")
---
+++ pc_graph.gml
@@ -0,0 +1,43 @@
+graph [
+  directed 1
+  node [
+    id 0
+    label "x1"
+  ]
+  node [
+    id 1
+    label "x2"
+  ]
+  node [
+    id 2
+    label "x3"
+  ]
+  node [
+    id 3
+    label "x4"
+  ]
+  node [
+    id 4
+    label "x5"
+  ]
+  edge [
+    source 0
+    target 2
+    directed 1
+  ]
+  edge [
+    source 0
+    target 3
+    directed 1
+  ]
+  edge [
+    source 1
+    target 2
+    directed 1
+  ]
+  edge [
+    source 2
+    target 4
+    directed 1
+  ]
+]
